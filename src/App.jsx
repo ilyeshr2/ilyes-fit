6b8a7ff3bdd472b08ce031aaceaf1675d355803f
ilyeshr2 <128542440+ilyeshr2@users.noreply.github.com>
2024-08-13 11:23:30 -0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import Hero from "./components/Hero"
+import Generator from "./components/Generator"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
+    <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
+    <Hero />
+    <Generator />
+    </main>
   )
 }
 
